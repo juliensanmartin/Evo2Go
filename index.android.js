7a390176a0a8b6169e4ae17d4120827562a097a5
Simon Reggiani <simon.reggiani@gmail.com>
2017-03-26 09:08:48 -0700

    auto indent code

--- index.android.js
+++ index.android.js
@@ -4,7 +4,10 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {
+  Component
+}
+from 'react';
 import {
   AppRegistry,
   StyleSheet,
@@ -12,15 +15,16 @@ import {
   Button,
   Alert,
   View
-} from 'react-native';
+}
+from 'react-native';
 import MapView from 'react-native-maps';
-
-
 
 export default class Evo2go extends Component {
   constructor(props) {
     super(props);
-    this.state = {markers: []};
+    this.state = {
+      markers: []
+    };
 
     // start to load the vehicles
     this.loadVehicles();
@@ -32,51 +36,60 @@ export default class Evo2go extends Component {
   }
 
   render() {
-    return (
-      <View style={styles.container}>
-        <MapView style={styles.map}
-            initialRegion={{
-              latitude: 49.2800565,
-              longitude: -123.1212937,
-              latitudeDelta: 0.0922,
-              longitudeDelta: 0.0421,
-            }}
-          >
-              {this.state.markers.map(marker => (
-                  <MapView.Marker
-                    key={marker.id}
-                    coordinate={marker.latlng}
-                  />
-                ))}
-          </MapView>
-      </View>
+    return ( < View style = {
+        styles.container
+      } >
+      < MapView style = {
+        styles.map
+      }
+      initialRegion = {
+        {
+          latitude: 49.2800565,
+          longitude: -123.1212937,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }
+      } > {
+        this.state.markers.map(marker => ( < MapView.Marker key = {
+            marker.id
+          }
+          coordinate = {
+            marker.latlng
+          }
+          />
+        ))
+      } < /MapView> < /View>
     );
   }
 
   loadVehicles() {
-      //console.log(this);
-      fetch('http://www.car2go.com/api/v2.1/vehicles?loc=vancouver&oauth_consumer_key=TransitApp&format=json')
-            .then((response) => response.json())
-            .then((responseJson) => {
-              return responseJson.placemarks;
-            })
-          .then((placemarks) => {
-                let markers = [];
+    //console.log(this);
+    fetch(
+        'http://www.car2go.com/api/v2.1/vehicles?loc=vancouver&oauth_consumer_key=TransitApp&format=json'
+      )
+      .then((response) => response.json())
+      .then((responseJson) => {
+        return responseJson.placemarks;
+      })
+      .then((placemarks) => {
+        let markers = [];
 
-                placemarks.map((placemark) => {
-                    markers.push({
-                            id: markers.length,
-                            latlng: {
-                                      latitude: placemark.coordinates[1],
-                                      longitude: placemark.coordinates[0]
-                                      }
-                                });
-                });
-                 this.setState({markers: markers});
-           })
-           .catch((error) => {
-             console.error(error);
-           });
+        placemarks.map((placemark) => {
+          markers.push({
+            id: markers.length,
+            latlng: {
+              latitude: placemark.coordinates[1],
+              longitude: placemark.coordinates[0]
+            }
+          });
+        });
+        this.setState({
+          markers: markers
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
 }
 
@@ -88,8 +101,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#F5FCFF',
   },
   map: {
-      ...StyleSheet.absoluteFillObject,
-    },
+    ...StyleSheet.absoluteFillObject,
+  },
   load_vehicules_btn: {
 
   },
