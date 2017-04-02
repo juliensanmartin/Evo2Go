6a0595c7f3f2e529a31ba57c881a6f3887be744b
Simon Reggiani <simon.reggiani@gmail.com>
2017-04-02 11:19:13 -0700

    fixed index.android.js

--- index.android.js
+++ index.android.js
@@ -1,4 +1,4 @@
 import React, { AppRegistry } from 'react-native';
 import Evo2Go from './src/App';
 
-AppRegistry.registerComponent('Evo2Go', () => Evo2Go);
+AppRegistry.registerComponent('evo2go', () => Evo2Go);
