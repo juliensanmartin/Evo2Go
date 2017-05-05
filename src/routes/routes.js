8f6184a7bcabe939999afbe38d426e1cdeed5b1e
Julien Sanmartin <sanmartinjulien@gmail.com>
2017-05-05 16:30:48 -0700

    En chantier!!

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import { StackNavigator } from 'react-navigation';
+import { TabNavigator, StackNavigator } from 'react-navigation';
 import { Icon } from 'react-native-elements';
 
 import Home from '../screens/Home/index';
 
+export const Tabs = TabNavigator({
+  Filter: {
+    screen: FeedStack,
+    navigationOptions: {
+      tabBarLabel: 'Feed',
+      tabBarIcon: ({ tintColor }) => <Icon name="list" size={35} color={tintColor} />,
+    },
+  },
+  Me: {
+    screen: Me,
+    navigationOptions: {
+      tabBarLabel: 'Me',
+      tabBarIcon: ({ tintColor }) => <Icon name="account-circle" size={35} color={tintColor} />
+    },
+  },
+});
+
 export default StackNavigator({
   Home: {
     screen: Home,
-  },
+  },{
+    mode: 'modal',
+    headerMode: 'none',
+  }
 })
