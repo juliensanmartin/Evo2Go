365e1d934338d0327bfd1fda1957f3b5aed65aee
Julien Sanmartin <sanmartinjulien@gmail.com>
2017-05-09 16:41:50 -0700

    Remove assets and try drawer

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,20 +1,35 @@
 import React from 'react'
-import { TabNavigator, StackNavigator } from 'react-navigation'
+import { TabNavigator, StackNavigator, DrawerNavigator } from 'react-navigation'
 import { Icon } from 'react-native-elements';
 
-import Home from '../screens/Home/index'
-import Filter from '../screens/Filter/index'
+import HomeScreen from '../screens/Home/index'
+import FilterScreen from '../screens/Filter/index'
+
+const drawerRight = DrawerNavigator({
+  Filter: { screen: FilterScreen }
+}, {
+    drawerPosition: 'right',
+    drawerWidth: 200
+})
+
+const homeStack = StackNavigator({
+  Home: { screen: HomeScreen }
+})
+
+const filterStack = StackNavigator({
+  drawerFilter: { screen: drawerRight }
+})
 
 export default TabNavigator({
-  Home: {
-    screen: Home,
+  tabHome: {
+    screen: homeStack,
     navigationOptions: {
       tabBarLabel: 'Map',
       tabBarIcon: ({ tintColor }) => <Icon type='font-awesome' size={ 20 } name='map' color={ tintColor }/>
     },
   },
-  Filter: {
-    screen: Filter,
+  tabFilter: {
+    screen: filterStack,
     navigationOptions: {
       tabBarLabel: 'Filter',
       tabBarIcon: ({ tintColor }) => <Icon type='font-awesome' size={ 20 } name='sliders' color={ tintColor }/>
