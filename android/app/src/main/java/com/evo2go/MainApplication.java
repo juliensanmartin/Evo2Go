4e7de439710e8a13adfdec7de42288505c84f796
Julien Sanmartin <sanmartinjulien@gmail.com>
2017-05-16 07:05:54 -0700

    install lottie for react native

--- android/app/src/main/java/com/evo2go/MainApplication.java
+++ android/app/src/main/java/com/evo2go/MainApplication.java
@@ -3,6 +3,7 @@ package com.evo2go;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.airbnb.android.react.lottie.LottiePackage;
 import com.wix.interactable.Interactable;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.airbnb.android.react.maps.MapsPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new LottiePackage(),
             new Interactable(),
             new VectorIconsPackage(),
             new MapsPackage()
