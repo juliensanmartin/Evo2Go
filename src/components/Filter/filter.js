9bb31a802f1078198c83121b30ab41af11680cb6
Julien Sanmartin <sanmartinjulien@gmail.com>
2017-05-31 21:41:56 -0700

    add car2go logo on filter

--- src/components/Filter/filter.js
+++ src/components/Filter/filter.js
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes } from 'react'
-import { View, Switch, Text } from 'react-native'
+import { View, Switch, Text, Image } from 'react-native'
 import styled from 'styled-components/native'
 
 export default class FilterComponent extends Component {
@@ -7,16 +7,23 @@ export default class FilterComponent extends Component {
     return (
       <StyledContainer>
         <StyledView>
+          <StyledLeftView>
+            <Text>EVO</Text>
+          </StyledLeftView>
           <StyledSwitch
             onValueChange={this.props.onEvoToggle}
             value={this.props.evoVisible} />
-          <Text>EVO</Text>
         </StyledView>
         <StyledView>
+          <StyledLeftView>
+            <StyledImage
+              source={require('./assets/Car2go_logo.png')}
+            />
+            <Text>CAR2GO</Text>
+          </StyledLeftView>
           <StyledSwitch
             onValueChange={this.props.onCar2GoToggle}
             value={this.props.car2GoVisible} />
-          <Text>CAR2GO</Text>
         </StyledView>
       </StyledContainer>
     )
@@ -45,7 +52,18 @@ const StyledView = styled.View`
   alignItems: center;
 `
 
+const StyledLeftView = styled.View`
+  flexDirection: row;
+  justifyContent: space-between;
+`
+
 const StyledSwitch= styled.Switch`
   padding: 10;
   margin: 10;
+`
+
+const StyledImage= styled.Image`
+  height: 20;
+  width: 20;
+  marginHorizontal: 10;
 `
