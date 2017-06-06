719743bd66f9b4d0990625ea901fcdc704309274
Julien Sanmartin <sanmartinjulien@gmail.com>
2017-06-06 10:17:49 -0700

    use icon to mark car location

--- src/components/IconMarker/icon-marker.js
+++ src/components/IconMarker/icon-marker.js
@@ -3,13 +3,13 @@ import { Icon } from 'react-native-elements'
 
 export default class IconMarkerComponent extends Component {
   render() {
-    const { type } = this.props.navigation.state.params.marker
+    const { type } = this.props.marker
 
     let color
-    if (type==='evoPin') color='#00a0e1'
+    if (type==='evoPin') color='black'
     if (type==='car2GoPin') color='#00BCE2'
     return (
-      <Icon type='ionicon' size={ 10 } name='model-s' color={color}/>
+      <Icon type='ionicon' size={ 20 } name='ios-car' color={color}/>
     )
   }
 }
